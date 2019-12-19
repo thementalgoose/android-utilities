2a920d8faf7ccbe0970df851622097116168c08e
Jordan Fisher <thementalgoose@gmail.com>
2019-12-19 23:26:16 +0000

    Increased Java support for Shared Prefs Util

--- library/src/main/java/tmg/utilities/utils/SharedPreferencesUtils.kt
+++ library/src/main/java/tmg/utilities/utils/SharedPreferencesUtils.kt
@@ -11,63 +11,75 @@ class SharedPreferencesUtils {
         //region Primitives
 
         @JvmStatic
+        @JvmOverloads
         fun save(context: Context, key: String, value: String, prefsKey: String = SHARED_PREFS_KEY) {
             edit(context, prefsKey).putString(key, value).apply()
         }
 
         @JvmStatic
+        @JvmOverloads
         fun save(context: Context, key: String, value: Boolean, prefsKey: String = SHARED_PREFS_KEY) {
             edit(context, prefsKey).putBoolean(key, value).apply()
         }
 
         @JvmStatic
+        @JvmOverloads
         fun save(context: Context, key: String, value: Int, prefsKey: String = SHARED_PREFS_KEY) {
             edit(context, prefsKey).putInt(key, value).apply()
         }
 
         @JvmStatic
+        @JvmOverloads
         fun save(context: Context, key: String, value: Float, prefsKey: String = SHARED_PREFS_KEY) {
             edit(context, prefsKey).putFloat(key, value).apply()
         }
 
         @JvmStatic
+        @JvmOverloads
         fun save(context: Context, key: String, value: Set<String>, prefsKey: String = SHARED_PREFS_KEY) {
             edit(context, prefsKey).putStringSet(key, value).apply()
         }
 
         @JvmStatic
+        @JvmOverloads
         fun save(context: Context, key: String, value: Long, prefsKey: String = SHARED_PREFS_KEY) {
             edit(context, prefsKey).putLong(key, value).apply()
         }
 
         @JvmStatic
-        fun getString(context: Context, key: String, prefsKey: String = SHARED_PREFS_KEY): String {
-            return prefs(context, prefsKey).getString(key, "") ?: ""
+        @JvmOverloads
+        fun getString(context: Context, key: String, defaultValue: String = "", prefsKey: String = SHARED_PREFS_KEY): String {
+            return prefs(context, prefsKey).getString(key, defaultValue) ?: defaultValue
         }
 
         @JvmStatic
-        fun getInt(context: Context, key: String, prefsKey: String = SHARED_PREFS_KEY): Int {
-            return prefs(context, prefsKey).getInt(key, -1)
+        @JvmOverloads
+        fun getInt(context: Context, key: String, defaultValue: Int = -1, prefsKey: String = SHARED_PREFS_KEY): Int {
+            return prefs(context, prefsKey).getInt(key, defaultValue)
         }
 
         @JvmStatic
-        fun getLong(context: Context, key: String, prefsKey: String = SHARED_PREFS_KEY): Long {
-            return prefs(context, prefsKey).getLong(key, -1L)
+        @JvmOverloads
+        fun getLong(context: Context, key: String, defaultValue: Long = -1L, prefsKey: String = SHARED_PREFS_KEY): Long {
+            return prefs(context, prefsKey).getLong(key, defaultValue)
         }
 
         @JvmStatic
-        fun getFloat(context: Context, key: String, prefsKey: String = SHARED_PREFS_KEY): Float {
-            return prefs(context, prefsKey).getFloat(key, -1f)
+        @JvmOverloads
+        fun getFloat(context: Context, key: String, defaultValue: Float = -1f, prefsKey: String = SHARED_PREFS_KEY): Float {
+            return prefs(context, prefsKey).getFloat(key, defaultValue)
         }
 
         @JvmStatic
-        fun getBoolean(context: Context, key: String, prefsKey: String = SHARED_PREFS_KEY): Boolean {
-            return prefs(context, prefsKey).getBoolean(key, false)
+        @JvmOverloads
+        fun getBoolean(context: Context, key: String, defaultValue: Boolean = false, prefsKey: String = SHARED_PREFS_KEY): Boolean {
+            return prefs(context, prefsKey).getBoolean(key, defaultValue)
         }
 
         @JvmStatic
-        fun getSet(context: Context, key: String, prefsKey: String = SHARED_PREFS_KEY): Set<String> {
-            return prefs(context, prefsKey).getStringSet(key, setOf()) ?: setOf()
+        @JvmOverloads
+        fun getSet(context: Context, key: String, defaultValue: Set<String> = setOf(), prefsKey: String = SHARED_PREFS_KEY): Set<String> {
+            return prefs(context, prefsKey).getStringSet(key, defaultValue) ?: defaultValue
         }
 
         //endregion
