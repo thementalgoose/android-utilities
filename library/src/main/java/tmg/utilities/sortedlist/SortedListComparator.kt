a3d2add56456b97dc1fcc6ed8d4d13106a24e0be
Jordan Fisher <thementalgoose@gmail.com>
2021-05-04 18:25:34 +0100

    Removed Sorted list adapter from utilities

--- library/src/main/java/tmg/utilities/sortedlist/SortedListComparator.kt
+++
@@ -1,6 +0,0 @@
-package tmg.utilities.sortedlist
-
-@Deprecated("SortedListAdapter is deprecated, please use a DiffList")
-interface SortedListComparator<E> : Comparator<E> {
-    fun equal(obj1: E, obj2: E): Boolean
-}
