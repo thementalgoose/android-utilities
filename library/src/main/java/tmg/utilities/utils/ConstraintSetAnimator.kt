c16f0d4df97d584853b0100abdfa9dbf286d7160
Jordan Fisher <thementalgoose@gmail.com>
2021-10-09 18:49:34 +0100

    Removed constraint set animator implementation in old package

--- library/src/main/java/tmg/utilities/utils/ConstraintSetAnimator.kt
+++ library/src/main/java/tmg/utilities/utils/ConstraintSetAnimator.kt
@@ -29,44 +29,13 @@ open class ConstraintSetAnimator(
 ) {
 
     var isAnimated: Boolean = false
-    private var originalConstraintSet: ConstraintSet = ConstraintSet()
-    private var modifiedConstraintSet: ConstraintSet = ConstraintSet()
 
     init {
-        originalConstraintSet.clone(layout)
-        modifiedConstraintSet.clone(originalConstraintSet)
-        initialiseNewLayout(modifiedConstraintSet)
     }
 
-    fun revert() {
-        if (!isAnimated)
-            return
-        beginTransition()
-        originalConstraintSet.applyTo(layout)
-        isAnimated = false
-    }
+    fun revert() { /* No op */ }
 
-    fun animate() {
-        if (isAnimated)
-            return
-        beginTransition()
-        modifiedConstraintSet.applyTo(layout)
-        isAnimated = true
-    }
+    fun animate() { /* No op */ }
 
-    fun toggle() {
-        if (isAnimated) {
-            revert()
-        }
-        else {
-            animate()
-        }
-    }
-
-    private fun beginTransition() {
-        val transition = AutoTransition()
-        transition.duration = duration.toLong()
-        transition.interpolator = DecelerateInterpolator()
-        TransitionManager.beginDelayedTransition(layout, transition)
-    }
+    fun toggle() { /* No op */ }
 }
