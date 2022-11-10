701e3d10c9b90cdaffe4cc356682e48af923125c
Jordan Fisher <thementalgoose@gmail.com>
2022-11-10 15:58:26 +0000

    Deprecated Event and DataEvent

--- library/src/main/java/tmg/utilities/lifecycle/Event.kt
+++ library/src/main/java/tmg/utilities/lifecycle/Event.kt
@@ -1,5 +1,10 @@
 package tmg.utilities.lifecycle
 
+@Deprecated(
+    message = "The usage of single time events to the UI layer is discouraged as of https://developer.android.com/topic/architecture/ui-layer/events. In the event you do need this functionality, please use SingleLiveEvent",
+    replaceWith = ReplaceWith("SingleLiveEvent"),
+    level = DeprecationLevel.WARNING
+)
 open class Event {
     var processEvent: Boolean = true
         private set
